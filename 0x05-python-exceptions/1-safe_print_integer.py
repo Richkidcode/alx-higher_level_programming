3e65f6fada4115495180830573953e2637569560
Felix Kamau Kinyuru <144423962+Richkidcode@users.noreply.github.com>
2024-01-23 18:37:58 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (ValueError, TypeError):
         return False
-
